960ab4c7226ded013ead177ea373c923a8c20513
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2021-06-08 15:41:50 +0530

    Create Write a program to cyclically rotate an array by one.cpp

---
+++ Questions-by-Love-Babbar/Array/Write a program to cyclically rotate an array by one.cpp
@@ -0,0 +1,38 @@
+// Question
+// Given an array, rotate the array by one position in anticlock-wise direction.
+  
+//---------------------Code----------------------------
+// { Driver Code Starts
+//Initial Template for C++
+#include <stdio.h>
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+
+//User function Template for C++
+void rotate(int arr[], int n)
+{
+    int temp = arr[n-1];
+    for(int i=n-1; i>=1; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = temp;
+}
