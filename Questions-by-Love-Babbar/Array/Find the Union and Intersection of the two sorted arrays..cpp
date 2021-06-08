174b9920b04a487644c8d4f477a46f73b8c8d3cf
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2021-06-08 15:34:00 +0530

    Create Find the Union and Intersection of the two sorted arrays..cpp

---
+++ Questions-by-Love-Babbar/Array/Find the Union and Intersection of the two sorted arrays..cpp
@@ -0,0 +1,47 @@
+// Question
+// Given two arrays a[] and b[] of size n and n respectively. The task is to find union between these two arrays.
+// Union of the two arrays can be defined as the set containing distinct elements from both the arrays.
+// If there are repetitions, then only one occurrence of element should be printed in union.
+
+//---------Code---------------
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        map<int, int> mp;
+        for(int i=0; i<n; i++){
+            mp.insert(pair<int, int>(a[i],1));
+        }
+        for(int i=0; i<m; i++){
+            mp.insert(pair<int, int>(b[i],1));
+        }
+        return mp.size();
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int t;
+	cin >> t;
+	while(t--){	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;    
+	}	
+	return 0;
+}  // } Driver Code Ends
