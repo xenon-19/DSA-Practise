104d7e6d89a64e01edebfd369b0d06b90cdf16e9
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2021-06-13 17:21:03 +0530

    Update Find the Union and Intersection of the two sorted arrays..cpp

--- Questions-by-Love-Babbar/Array/Find the Union and Intersection of the two sorted arrays..cpp
+++ Questions-by-Love-Babbar/Array/Find the Union and Intersection of the two sorted arrays..cpp
@@ -6,42 +6,50 @@
 //---------Code---------------
 // { Driver Code Starts
 //Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
-
  // } Driver Code Ends
 //User function template in C++
-
 class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
     int doUnion(int a[], int n, int b[], int m)  {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
+        int cnt=0;
         for(int i=0; i<n; i++){
-            mp.insert(pair<int, int>(a[i],1));
+            if(mp.find(a[i])==mp.end()){
+                mp.insert(pair<int, int>(a[i],1));
+                cnt++;
+            }
         }
         for(int i=0; i<m; i++){
-            mp.insert(pair<int, int>(b[i],1));
+            if(mp.find(b[i])==mp.end()){
+                mp.insert(pair<int, int>(b[i],1));
+                cnt++;
+            }
         }
-        return mp.size();
+        return cnt;
     }
 };
 
 // { Driver Code Starts.
-int main() {
+
+int main() {	
 	int t;
-	cin >> t;
-	while(t--){	    
+	cin >> t;	
+	while(t--){    
 	    int n, m;
 	    cin >> n >> m;
-	    int a[n], b[m];	   
+	    int a[n], b[m];
+	   
 	    for(int i = 0;i<n;i++)
-	       cin >> a[i];	       
+	       cin >> a[i];
+	       
 	    for(int i = 0;i<m;i++)
 	       cin >> b[i];
 	    Solution ob;
-	    cout << ob.doUnion(a, n, b, m) << endl;    
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
 	}	
 	return 0;
 }  // } Driver Code Ends
