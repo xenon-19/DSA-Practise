0b602d0878556e32873ccb327db7ed8cf82651e8
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2021-07-06 19:34:15 +0530

    Update Write a Code to check whether one string is a rotation of another.cpp

--- Questions-by-Love-Babbar/String/Write a Code to check whether one string is a rotation of another.cpp
+++ Questions-by-Love-Babbar/String/Write a Code to check whether one string is a rotation of another.cpp
@@ -3,7 +3,7 @@ https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-eac
 //Given a string s1 and a string s2, write a snippet to say whether s2 is a rotation of s1?
 
 //-------Code-------
-//-------O(n^2)
+//-------O(n^2)-naive approach
 #include <iostream>
 using namespace std;
 
@@ -28,5 +28,64 @@ int main() {
 	string str1 = "abcde", str2 = "deabc";
 	int n = str1.length(), m = str2.length();
 	cout << areRotation(str1, str2, n, m);
+	return 0;
+}
+
+//-------O(n),O(n)---KMP algo
+#include <iostream>
+using namespace std;
+
+int* make_lcs(string s) {
+	int n = s.length(), i = 0, j = 1;
+	int* lcs = new int[n];
+	lcs[0] = 0;
+
+	while (j < n) {
+		if (s[i] == s[j]) {
+			lcs[j] = i + 1;
+			i++; j++;
+		}
+		else {
+			if (i == 0) {
+				lcs[j] = 0;
+				j++;
+			}
+			else {
+				i = lcs[i - 1];
+			}
+		}
+	}
+	return lcs;
+}
+
+bool checkIfRotation(string s1, string s2) {
+	int n = s1.length();
+	int m = s2.length();
+	if (n != m) return false;
+	if (m == 0) return false;
+
+	int* lcs = make_lcs(s2);
+
+	int i = 0, j = 0;
+	while (i < 2 * n) {
+		if (s1[i % n] == s2[j]) {
+			if (j == m - 1) return true;
+			i++; j++;
+		}
+		else {
+			if (j == 0) i++;
+			else j = lcs[j - 1];
+		}
+	}
+
+	return false;
+}
+
+int main() {
+	string s1 = "acfgtr";
+	string s2 = "racfgt";
+
+	cout << checkIfRotation(s1, s2);
+
 	return 0;
 }
