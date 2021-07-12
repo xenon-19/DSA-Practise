13bf044a0449ffe3edf00e1d9e20fe5ec989bce3
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2021-07-12 21:44:01 +0530

    Update Count set bits in an integer.cpp

--- Questions-by-Love-Babbar/Bit Manipulation/Count set bits in an integer.cpp
+++ Questions-by-Love-Babbar/Bit Manipulation/Count set bits in an integer.cpp
@@ -35,3 +35,39 @@ int main() {
     return 0;
 }
   // } Driver Code Ends
+
+//---------Code--------
+//number of times loop runs is number of set bits in n
+//Brian Kernighan's Algorithm
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        int cnt = 0;
+        while(N > 0){
+            N = (N-1)&N;
+            cnt++;
+        }
+        return cnt;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
