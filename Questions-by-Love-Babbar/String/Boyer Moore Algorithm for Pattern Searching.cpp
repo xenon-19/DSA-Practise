f645cf2b6aed799dbd9c4e4ca01fe0be0152dbef
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2021-09-17 23:19:38 +0530

    Update Boyer Moore Algorithm for Pattern Searching.cpp

--- Questions-by-Love-Babbar/String/Boyer Moore Algorithm for Pattern Searching.cpp
+++ Questions-by-Love-Babbar/String/Boyer Moore Algorithm for Pattern Searching.cpp
@@ -1,8 +1,13 @@
 // Boyer Moore Algorithm for Pattern Searching.
+//Best expanation I found -> https://www.inf.hs-flensburg.de/lang/algorithmen/pattern/bmen.htm
+//.htm not .html
+
+//2 parts, bad-character, good-suffix, both should be combined for faster search
+//worst case -> m*n
+//best-case -> n/m
 
 //-----------Code-----------
 //---Bad Character Heuristic
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -51,4 +56,74 @@ int main() {
 	return 0;
 }
 
-  
+//----Good Suffix Heuristic
+#include <bits/stdc++.h>
+using namespace std;
+
+void preprocess(string &pattern, vector<int> &shift){
+      //case1
+      //let temp be suffix of pattern startig at pos i
+      //bpos[i]=starting pos of longest suffix of temp which is also a prefix
+      vector<int> bpos(pattern.size()+1);
+      bpos[pattern.size()] = pattern.size()+1;
+      int i=pattern.size()-1, j=pattern.size();
+      while(i>=0){
+            while(j<pattern.size() && pattern[i] != pattern[j]){
+                  if(!shift[j]){
+                        shift[j] = j-i;
+                  }
+                  j = bpos[j+1] - 1;
+            }
+            bpos[i]=j;
+            i--; j--;
+      }
+      
+      //case2
+      //where a suffix of matched suffix is a prefix of pattern
+      //refer to above link for more detailed explanation
+      j = bpos[0];
+      for(int i=0; i<pattern.size(); i++){
+            //if i has reached j, then push j forward
+            if(i==j) j=bpos[j];
+            
+            //if shift[i]==0 => case2 situation
+            if(shift[i]==0) shift[i] = j;
+      }
+}
+
+vector<int> find_pat(string &text, string &pattern){
+      //stores pos of match
+      vector<int> pos;
+      
+      //shift[i] stores how much to shift if mismatch occurs at i
+      vector<int> shift(pattern.size(), 0);
+      preprocess(pattern, shift);
+      
+      //initial shift of pattern, to match with text
+      int s=0;
+      
+      while(s+pattern.size() <= text.size()){
+            int i=pattern.size()-1;
+            while(i>=0){
+                  if(text[s+i] == pattern[i])     i--;
+                  else break;
+            }
+            if(i==-1){
+                  pos.push_back(s);
+                  s++;
+            }else s+=shift[i];
+      }
+      
+      return pos;
+}
+
+//driver code
+int main() {
+	string text = "wagwegiagweagwegguaagweggweggagwegbwiagwegovnwo";
+	string pattern = "agweg";
+	vector<int> occurences = find_pat(text, pattern);
+	for(auto x: occurences){
+	      cout << x << " ";
+	}cout << endl;
+	return 0;
+}
