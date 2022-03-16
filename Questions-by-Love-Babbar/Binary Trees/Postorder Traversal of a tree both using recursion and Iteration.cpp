3a591c05c45ec8a15a18bae19bb651a76147685f
Nishiket Bidawat <77715807+xenon-19@users.noreply.github.com>
2022-03-16 12:49:35 +0530

    Update Postorder Traversal of a tree both using recursion and Iteration.cpp

--- Questions-by-Love-Babbar/Binary Trees/Postorder Traversal of a tree both using recursion and Iteration.cpp
+++ Questions-by-Love-Babbar/Binary Trees/Postorder Traversal of a tree both using recursion and Iteration.cpp
@@ -18,4 +18,27 @@ vector <int> postOrder(Node* root)
 }
 
 //--------------------iterative--------------------------
+vector <int> postOrder(Node* root)
+{
+  stack<Node*> temp;
+  stack<int> vis;
+  vector<int> ans;
+  
+  Node* curr = root;
+  
+  temp.push(curr);
+  while(!temp.empty()){
+      curr = temp.top();
+      temp.pop();
+      vis.push(curr->data);
+      if(curr->left) temp.push(curr->left);
+      if(curr->right) temp.push(curr->right);
+  }
+  
+  while(!vis.empty()){
+      ans.push_back(vis.top());
+          vis.pop();
+  }
+  return ans;
 
+}
